77e4021281ffe1393143fd0ccbcbaffa43343679
Breno Viana <brenomfviana@gmail.com>
2017-10-18 11:52:06 -0300

    Adds project files.

---
+++ rita/app/src/main/java/com/tb/rita/delivery/SettingsActivity.java
@@ -0,0 +1,15 @@
+package com.tb.rita.delivery;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.tb.rita.R;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+    }
+}
