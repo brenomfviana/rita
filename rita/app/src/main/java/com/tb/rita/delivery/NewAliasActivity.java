07208d2afe51672e84abf3d273e3496f2bf3a8e0
Thales Aguiar <thalesaguiar21@gmail.com>
2017-11-04 12:49:30 -0300

    Fixed New Alias screen layout

--- rita/app/src/main/java/com/tb/rita/delivery/NewAliasActivity.java
+++ rita/app/src/main/java/com/tb/rita/delivery/NewAliasActivity.java
@@ -1,15 +1,26 @@
 package com.tb.rita.delivery;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import com.tb.rita.R;
 
 public class NewAliasActivity extends AppCompatActivity {
 
+    public static final String NEW_ALIAS_NAME = "INPUT FROM TEXT FIELD";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.new_alias_screen);
+    }
+
+
+    /* Transition functions */
+    public void onBackButtonPressed(View view) {
+        Intent toCmdDescr = new Intent(this, NewAliasActivity.class);
+        startActivity(toCmdDescr);
     }
 }
