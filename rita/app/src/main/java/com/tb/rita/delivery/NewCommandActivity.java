b348e98588972c317a433c0acb2767dd3b1f795c
Thales Aguiar <thalesaguiar21@gmail.com>
2017-11-06 21:07:46 -0300

    Add command screen now uses a radio group with appliances values

--- rita/app/src/main/java/com/tb/rita/delivery/NewCommandActivity.java
+++ rita/app/src/main/java/com/tb/rita/delivery/NewCommandActivity.java
@@ -4,8 +4,16 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 import com.tb.rita.R;
+
+import java.util.List;
+
+import domain.Appliance;
+
 /**
  * Created by thales on 04/11/17.
  */
@@ -17,6 +25,19 @@ public class NewCommandActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         // Set content
         setContentView(R.layout.new_command_screen);
+
+        populateAppliances();
+    }
+
+    private void populateAppliances() {
+        RadioGroup radios = (RadioGroup) findViewById(R.id.ncmd_appliances);
+        Appliance appliances[] = Appliance.values();
+        for(int i = 0; i < appliances.length; i++) {
+            RadioButton rb = new RadioButton(this);
+            rb.setText(appliances[i].toString());
+            rb.setId(i);
+            radios.addView(rb);
+        }
     }
 
     public void OnBackButtonPressed(View view) {
--- rita/app/src/main/java/domain/Appliance.java
+++ rita/app/src/main/java/domain/Appliance.java
@@ -8,4 +8,17 @@ public enum Appliance {
     FAN,
     HEATER,
     TV;
+
+    @Override
+    public String toString() {
+        String str = "";
+        if(this == FAN) {
+            str = "FAN";
+        } else if(this == HEATER) {
+            str = "HEATER";
+        } else if(this == TV) {
+            str = "TV";
+        }
+        return str;
+    }
 }
