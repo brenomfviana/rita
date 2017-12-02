5ae36b876fd2cf95f844f311aaf1e7b15fae14a1
Thales Aguiar <thalesaguiar21@gmail.com>
2017-12-02 01:14:20 -0300

    Added speech recognition to New Command Screen

--- rita/app/src/main/java/com/tb/rita/delivery/NewCommandActivity.java
+++ rita/app/src/main/java/com/tb/rita/delivery/NewCommandActivity.java
@@ -1,23 +1,30 @@
 package com.tb.rita.delivery;
 
+import android.bluetooth.BluetoothAdapter;
+import android.content.ActivityNotFoundException;
 import android.content.Context;
 import android.content.Intent;
 import android.content.res.ColorStateList;
 import android.os.Bundle;
+import android.speech.RecognizerIntent;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.Toast;
 
 import com.tb.rita.R;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 
 import domain.Alias;
 import domain.Appliance;
 import domain.Command;
+import domain.CommandGrammar;
 import domain.dao.AppDatabase;
 
 /**
@@ -26,11 +33,27 @@ import domain.dao.AppDatabase;
 
 public class NewCommandActivity extends AppCompatActivity {
 
+    ImageButton tapToTalk;
+    BluetoothService btService;
+    private final int REQUEST_SPEECH_RECOG = 1;
+    private final int REQUEST_BLUETOOTH_ON = 2;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         // Set content
         setContentView(R.layout.new_command_screen);
+
+        btService = new BluetoothService(this);
+        tapToTalk = findViewById(R.id.ncmd_tap_talk);
+
+        tapToTalk.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onTapToTalk(view);
+            }
+        });
+
         populateAppliances();
     }
 
@@ -136,6 +159,65 @@ public class NewCommandActivity extends AppCompatActivity {
             }
         }
         return isValid;
+    }
+
+    // ================================== Speech  Recognition Functions
+    public void onTapToTalk(View view) {
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+                RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+        intent.putExtra(RecognizerIntent.EXTRA_PROMPT, getString(R.string.speech_prompt_title));
+        try {
+            startActivityForResult(intent, REQUEST_SPEECH_RECOG);
+        } catch (ActivityNotFoundException a) {
+            Toast.makeText(getApplicationContext(),
+                    getString(R.string.unsuportted_speech),
+                    Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void checkBluetooth(String msg) {
+        btService = new BluetoothService(this);
+        if(!btService.isBluetoothEnabled()) {
+            Intent enableBluetooth = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableBluetooth, REQUEST_BLUETOOTH_ON);
+        } else {
+            // Bluetooth já está ativado
+            onSpeechFound(msg);
+        }
+    }
+
+    private void onSpeechFound(String text) {
+        List<Command> cmds = new ArrayList<>();
+        CommandGrammar cmdGrammar = new CommandGrammar(cmds);
+        String cmd = cmdGrammar.getValidCmdFromText(text);
+        btService.connectAndSend(cmd);
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        String msg = "";
+        switch (requestCode) {
+            case REQUEST_SPEECH_RECOG: {
+                if(resultCode == RESULT_OK) {
+                    ArrayList<String> mySpeech = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+                    if(mySpeech != null) {
+                        msg = mySpeech.get(0);
+                        checkBluetooth(msg);
+                    }
+                }
+                break;
+            }
+            case REQUEST_BLUETOOTH_ON: {
+                if(resultCode == RESULT_OK) {
+                    Toast.makeText(this, "Bluetooh ativado com sucesso!", Toast.LENGTH_LONG).show();
+                    onSpeechFound(msg);
+                } else {
+                    Toast.makeText(getApplicationContext(), "Não foi possível ativar o bluetooth", Toast.LENGTH_LONG).show();
+                }
+                break;
+            }
+        }
     }
 
 }
